8b6954f5cb31698fbd39d66698260574b035f08b
Caio_CPS <caio.psouza@robbyson.com>
2021-01-17 06:26:46 -0300

    feat: Criada rota de upload de avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -7,21 +7,33 @@ import {
   putValidBodyAndID,
 } from './routesValidations/userValidation';
 import UsersController from '../controllers/UsersController';
-import isAutenticated from '@shared/http/middlewares/isAuthenticated';
+import UsersAvatarController from '../controllers/UserAvatarController';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
-usersRouter.get('/', isAutenticated, usersController.index);
+const upload = multer(uploadConfig);
+
+usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.post(
   '/',
-  isAutenticated,
+  isAuthenticated,
   celebrate(postBody),
   usersController.create,
 );
 usersRouter.get('/:id', celebrate(getValidID), usersController.show);
 usersRouter.put('/:id', celebrate(putValidBodyAndID), usersController.update);
 usersRouter.delete('/:id', celebrate(deleteValidID), usersController.delete);
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
+);
 
 export default usersRouter;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,7 +2,7 @@ import { AppError } from '@shared/errors/AppError';
 import path from 'path';
 import fs from 'fs';
 import { getCustomRepository } from 'typeorm';
-import User from '@modules/users/typeorm/entities/User';
+import User from '../typeorm/entities/User';
 import UsersRepository from '@modules/users/typeorm/repositories/UsersRepositories';
 import uploadConfig from '@config/upload';
 
@@ -12,33 +12,29 @@ interface IRequest {
 }
 
 class UpdateUserAvatarService {
-  public async execute({
-    user_id,
-    avatarFilename,
-  }: IRequest): Promise<User | undefined> {
-    const usersRepositories = getCustomRepository(UsersRepository);
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    const user = await usersRepositories.findById(user_id);
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
-      throw new AppError(' User not found.');
+      throw new AppError('User not found.');
     }
 
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-
       const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
       if (userAvatarFileExists) {
         await fs.promises.unlink(userAvatarFilePath);
       }
-
-      user.avatar = avatarFilename;
-
-      await usersRepositories.save(user);
-
-      return user;
     }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
   }
 }
 
