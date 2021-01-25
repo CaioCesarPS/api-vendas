7f9b22462f4025dcb031a78082e1218974bb0e3e
Caio_CPS <caio.psouza@robbyson.com>
2021-01-25 17:16:02 -0300

    feat: Created Email forgot password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,33 +1,49 @@
 import { AppError } from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import UserRepository from '@modules/users/typeorm/repositories/UsersRepositories';
-import UsersTokensRepository from '@modules/users/typeorm/repositories/UsersTokensRepository';
+import path from 'path';
+import UsersRepository from '@modules/users/typeorm/repositories/UsersRepositories';
+import UserTokensRepository from '@modules/users/typeorm/repositories/UsersTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
+
 interface IRequest {
   email: string;
 }
 
-class CreateUserService {
+class SendForgotPasswordEmailService {
   public async execute({ email }: IRequest): Promise<void> {
-    const usersRepository = getCustomRepository(UserRepository);
-    const userTokenRepository = getCustomRepository(UsersTokensRepository);
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
 
     const user = await usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new AppError('User not found');
+      throw new AppError('User does not exists.');
     }
 
-    // console.log(user);
+    const { token } = await userTokensRepository.generate(user.id);
 
-    const userToken = await userTokenRepository.generate(user.id);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
-    // console.log(userToken);
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida ${userToken.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
     });
   }
 }
 
-export default CreateUserService;
+export default SendForgotPasswordEmailService;
