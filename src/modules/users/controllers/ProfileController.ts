ad3384a707baa8e939264290a63e122a2409387d
Caio_CPS <caio.psouza@robbyson.com>
2021-03-24 18:30:37 -0300

    improve: create a UpdateProfileUser controller

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import showProfileService from '@modules/users/services/ShowProfileService';
+import UpdateUserService from '@modules/users/services/UpdateProfileService';
+
+export default class UsersController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const showProfile = new showProfileService();
+    const user = await showProfile.execute({ user_id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, old_password } = request.body;
+    const user_id = request.user.id;
+
+    const updateUser = new UpdateUserService();
+    const update = await updateUser.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(update);
+  }
+}
